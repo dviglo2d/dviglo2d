90bbe5cdfa666bcd1487b931c1f3694a92dbfca8
1vanK <1vanK@users.noreply.github.com>
2023-04-17 10:09:24 +0300

    Добавил ScopeGuard

--- samples/hello/main.cpp
+++ samples/hello/main.cpp
@@ -3,6 +3,7 @@
 
 #include <dviglo/common/primitive_types.h>
 #include <dviglo/main/main.h>
+#include <dviglo/std_utils/scope_guard.h>
 
 #include <SDL.h>
 
@@ -11,6 +12,12 @@ using namespace dviglo;
 
 i32 run()
 {
+    // Вызываем SDL_Quit(), даже если SDL_Init() вернул ошибку
+    const ScopeGuard sg_sdl_quit = [] { SDL_Quit(); };
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+        return 1;
+
     SDL_Window* window = SDL_CreateWindowWithPosition(
         "Окно закроется через 3 секунды",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
@@ -19,6 +26,8 @@ i32 run()
 
     if (!window)
         return 1;
+
+    const ScopeGuard sg_destroy_window = [window] { SDL_DestroyWindow(window); };
 
     SDL_Delay(3000);
 
