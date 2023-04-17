d718f4257865725444f3870b5d79bfc01c3e0a1c
1vanK <1vanK@users.noreply.github.com>
2023-04-17 03:50:49 +0300

    config.h: Стиль

--- libs/dviglo/common/config.h
+++ libs/dviglo/common/config.h
@@ -5,15 +5,23 @@
 #pragma once
 
 #ifdef DV_SHARED // Динамическая версия движка
+
     #ifdef _WIN32 // Visual Studio или MinGW
+
         #ifdef DV_IS_BUILDING // Компиляция движка
             #define DV_API __declspec(dllexport)
         #else // Использование движка
             #define DV_API __declspec(dllimport)
         #endif
+
     #else // Linux
+
         #define DV_API __attribute__((visibility("default")))
+
     #endif
+
 #else // Статическая версия движка
+
     #define DV_API
-#endif
+
+#endif // def DV_SHARED
