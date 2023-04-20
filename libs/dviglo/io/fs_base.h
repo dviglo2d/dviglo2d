2f7d1c3f34f611a10bb0b47def667711b220d2ed
1vanK <1vanK@users.noreply.github.com>
2023-04-20 14:52:52 +0300

    Добавил fs_base

---
+++ libs/dviglo/io/fs_base.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2022-2023 the Dviglo project
+// License: MIT
+
+// Функции, которые можно использовать до инициализации любых подсистем
+
+#pragma once
+
+#include "../std_utils/str.h"
+
+
+namespace dviglo
+{
+
+DV_API bool dir_exists(StrViewUtf8 path);
+
+/// Версия функции, которая не пишет с лог
+DV_API bool create_dir_silent(StrViewUtf8 path);
+
+/// Аналог SDL_GetPrefPath(), который не требует инициализации SDL
+/// <https://github.com/libsdl-org/SDL/issues/2587>.
+/// org может быть "".
+/// В конце пути добавляет '/'.
+/// В случае неудачи возвращает пустую строку
+DV_API StrUtf8 get_pref_path(StrViewUtf8 org, StrViewUtf8 app);
+
+} // namespace dviglo
