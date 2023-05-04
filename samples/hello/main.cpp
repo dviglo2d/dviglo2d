c296fc441fbc7c49cfc3f92af956dde76c4f3f73
1vanK <1vanK@users.noreply.github.com>
2023-05-05 02:59:41 +0300

    Добавил библиотеку EnTT

--- samples/hello/main.cpp
+++ samples/hello/main.cpp
@@ -5,12 +5,45 @@
 #include <dviglo/main/main.h>
 #include <dviglo/std_utils/scope_guard.h>
 
+#include <entt/entt.hpp>
 #include <SDL.h>
 
 #include <memory>
 
 using namespace dviglo;
 using namespace std;
+
+
+struct position {
+    float x;
+    float y;
+};
+
+struct velocity {
+    float dx;
+    float dy;
+};
+
+void update(entt::registry& registry) {
+    auto view = registry.view<const position, velocity>();
+
+    // use a callback
+    view.each([](const auto& pos, auto& vel) { /* ... */ });
+
+    // use an extended callback
+    view.each([](const auto entity, const auto& pos, auto& vel) { /* ... */ });
+
+    // use a range-for
+    for (auto [entity, pos, vel] : view.each()) {
+        // ...
+    }
+
+    // use forward iterators and get only the components of interest
+    for (auto entity : view) {
+        auto& vel = view.get<velocity>(entity);
+        // ...
+    }
+}
 
 
 class App : public Application
@@ -44,6 +77,16 @@ i32 run()
         return 1;
 
     const ScopeGuard sg_destroy_window = [window] { SDL_DestroyWindow(window); };
+
+    entt::registry registry;
+
+    for (auto i = 0u; i < 10u; ++i) {
+        const auto entity = registry.create();
+        registry.emplace<position>(entity, i * 1.f, i * 1.f);
+        if (i % 2 == 0) { registry.emplace<velocity>(entity, i * .1f, i * .1f); }
+    }
+
+    update(registry);
 
     SDL_Delay(3000);
 
