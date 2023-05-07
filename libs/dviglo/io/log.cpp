7ab798ceecb2edbd3b458ac9557c58a972cddc24
1vanK <1vanK@users.noreply.github.com>
2023-05-07 12:42:56 +0300

    Добавил функцию time_to_str() и включил _CRT_SECURE_NO_WARNINGS

--- libs/dviglo/io/log.cpp
+++ libs/dviglo/io/log.cpp
@@ -12,20 +12,32 @@ using namespace std;
 namespace dviglo
 {
 
+static StrUtf8 time_to_str()
+{
+    time_t current_time = time(nullptr);
+    char tmp_buffer[sizeof "yyyy-mm-dd hh:mm:ss"];
+
+    // %F и %T не работают в MinGW
+    //strftime(tmp_buffer, sizeof tmp_buffer, "%F %T", localtime(&current_time));
+
+    strftime(tmp_buffer, sizeof tmp_buffer, "%Y-%m-%d %H:%M:%S", localtime(&current_time));
+    return StrUtf8(tmp_buffer);
+}
+
 Log::Log(StrViewUtf8 path)
 {
     assert(!log_);
 
     log_ = this;
 
-    cout << "Конструктор лога: " << path << endl;
+    cout << '[' << time_to_str() << "] Конструктор лога: " << path << endl;
 }
 
 Log::~Log()
 {
     log_ = nullptr;
 
-    cout << "Деструктор лога" << endl;
+    cout << '[' << time_to_str() << "] Деструктор лога" << endl;
 }
 
 } // namespace dviglo
