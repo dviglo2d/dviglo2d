a822d2e470e933e6c28ddb2e3eb0e10b42318eac
1vanK <1vanK@users.noreply.github.com>
2023-05-10 10:17:07 +0300

    Image: Запрещаем копирование

--- libs/dviglo/res/image.h
+++ libs/dviglo/res/image.h
@@ -19,6 +19,15 @@ public:
     Image(const StrUtf8& path);
     ~Image();
 
+    // Запрещаем копировать объект, так как если в одной из копий будет вызван деструктор,
+    // все другие объекты будут иметь ссылку на уничтоженный data_
+    Image(const Image&) = delete;
+    Image& operator=(const Image&) = delete;
+
+    // Но разрешаем перемещение, чтобы было можно хранить объект в векторе
+    Image(Image&&) = default;
+    Image& operator=(Image&&) = default;
+
     i32 width() const { return width_; }
     i32 height() const { return height_; }
     i32 num_components() const { return num_components_; }
