90ea1197a5450dd24c975afbdb4b06257ce79ada
1vanK <1vanK@users.noreply.github.com>
2024-03-09 12:49:22 +0300

    Используем стандартные типы с фиксированным размером
    Собственные типы тоже не решают всех проблем. Например следующий код будет компилироваться в Windows, но не будет в Linux, потому что в Windows число 0x7FFFFFFFFFFFFFFF имеет тип long long, а в Linux тип long:
    
    using i32 = int;
    using i64 = long long;
    
    struct S
    {
        S(i32 v) {}
        S(i64 v) {}
    };
    
    int main()
    {
        S s(0x7FFFFFFFFFFFFFFF);
    }

--- libs/dviglo/common/primitive_types.cpp
+++ libs/dviglo/common/primitive_types.cpp
@@ -1,9 +1,10 @@
 // Copyright (c) the Dviglo project
 // License: MIT
 
-#include <cstddef> // ptrdiff_t
-#include <cstdint> // intptr_t
-#include <limits>  // numeric_limits
+#include <cstddef>     // ptrdiff_t
+#include <cstdint>     // int64_t
+#include <limits>      // numeric_limits
+#include <type_traits> // is_same_v
 
 using namespace std;
 
@@ -15,13 +16,18 @@ static_assert(sizeof(int) == 4);
 static_assert(sizeof(long long) == 8);
 static_assert(sizeof(char32_t) == 4);
 
-#ifdef _WIN32 // Windows
+#ifdef _WIN32 // MSVC или MinGW
 static_assert(sizeof(long) == 4);
 static_assert(sizeof(wchar_t) == 2);
-#else // Unix
-static_assert(sizeof(long) == sizeof(void*)); // 4 или 8
+static_assert(is_same_v<int64_t, long long>);
+#else // GCC или Clang
+static_assert(sizeof(long) == sizeof(void*));
 static_assert(sizeof(wchar_t) == 4);
+static_assert(is_same_v<int64_t, long>);
 #endif
+
+// Тип 0x7FFFFFFFFFFFFFFF зависит от компилятора
+static_assert(is_same_v<decltype(0x7FFFFFFFFFFFFFFF), int64_t>);
 
 // Арифметика указателей
 static_assert(sizeof(void*) == sizeof(ptrdiff_t));
--- libs/dviglo/common/primitive_types.hpp
+++ libs/dviglo/common/primitive_types.hpp
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <cstddef> // std::byte
+#include <cstdint> // int8_t, ...
 
 
 // Пользователь может инжектировать dvt (dviglo types) в любое пространство имён.
@@ -12,15 +13,14 @@
 namespace dvt
 {
 
-// https://en.cppreference.com/w/cpp/language/types
-using i8 = signed char;
-using u8 = unsigned char;
-using i16 = short;
-using u16 = unsigned short;
-using i32 = int;
-using u32 = unsigned;
-using i64 = long long;
-using u64 = unsigned long long;
+using i8 = int8_t;
+using u8 = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
 using f32 = float;
 using f64 = double;
 
@@ -46,54 +46,10 @@ using flagset32 = u32;
 
 } // namespace dvt
 
+
 namespace dviglo
 {
 
 using namespace dvt;
 
 } // namespace dviglo
-
-/*
-
-# Мотивация
-
-У большинства современных компиляторов размеры примитивных типов совпадают (за исключением long).
-Но стандарт C++ этого не требует: <https://en.cppreference.com/w/cpp/language/types>.
-Поэтому распространённой практикой является использование типов с фиксированным размером (например Uint32 в SDL).
-
-# Чем плохи [стандартные типы с фиксированным размером](https://en.cppreference.com/w/cpp/types/integer)?
-
-Тип int64_t зависит от компилятора:
-* GCC 32, Clang 32, VS 32, VS 64 - long long
-* GCC 64, Clang 64 - long
-
-Следующий код успешно скомпилируют VS 32, VS 64 и GCC 32, но не скомпилирует GCC 64:
-
-```
-#include <cstdint>
-
-struct S
-{
-    S(int i32) {}
-    S(long long i64) {}
-};
-
-int main()
-{
-    int64_t x;
-    S s(x);
-}
-```
-
-GCC 64 просто не сможет выбрать, какой из двух конструкторов использовать.
-
-Проверить можно [тут](https://godbolt.org).
-
-Кроме того имена стандартных типов излишне длинные: имя uint32_t такое же длинное, как unsigned.
-
-Поэтому используем имена [как в Rust](https://doc.rust-lang.org/book/ch03-02-data-types.html).
-
-В Common Intermediate Language используются еще более короткие имена (i8 вместо i64),
-однако такой вариант встречается редко и может сбивать с толку: u8 - это байт или 64 бита?
-
-*/
