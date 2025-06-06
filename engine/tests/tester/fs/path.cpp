85bbb6303bb86dae45251a0f14955ed91b6cf582
1vanK <1vanK@users.noreply.github.com>
2025-06-06 10:59:38 +0300

    Update path.cpp

--- engine/tests/tester/fs/path.cpp
+++ engine/tests/tester/fs/path.cpp
@@ -75,18 +75,48 @@ void test_fs_path()
         assert(ext == "");
     }
 
-    // Проверяем, что стандартная библиотека работает предсказуемым образом
+    // Проверяем, что стандартная библиотека работает предсказуемым образом.
+    // Отличия Linux и Windows:
+    // 1) Linux: В именах файлов (или папок) разрешены любые символы, кроме / и '\0'.
+    //           \, z: - обычные имена файлов
+    //    Windows: \ и / - разделители, некоторые символы запрещены: https://ru.wikipedia.org/wiki/Имя_файла
+    // 2) Linux: root_name (имя корня) всегда пустое
+    //    Windows: \\server_name или z: - имена корней
+    // 3) В Linux регистр имеет значение
     {
-        string m = "/usr///\\\\////lib";
-        auto aa = fs::path(m).root_name();
-        auto bb = fs::path(m).root_directory();
-        auto cc = fs::path(m).root_path();
-        auto dd = fs::path("/usr\\///\\\\////lib").lexically_normal().string();
+
+
+        fs::path m("\\привет\\hello/");
+        auto aa = m.root_name();
+        auto bb = m.root_directory();
+        auto cc = m.root_path();
+        auto dd = fs::path("//\\usr\\///\\\\////lib/\\\\").lexically_normal().string();
 
         assert(fs::path("").root_name() == "");
         assert(fs::path("").root_directory() == "");
         assert(fs::path("").root_path() == "");
 
+        assert(fs::path("/").root_name() == "");
+        assert(fs::path("/").root_directory() == "/");
+        assert(fs::path("/").root_path() == "/");
+
+#if DV_WINDOWS
+        // Это разделитель
+        assert(fs::path("\\").root_name() == "");
+        assert(fs::path("\\").root_directory() == "\\");
+        assert(fs::path("\\").root_path() == "\\");
+        assert(fs::path("\\").filename() == "");
+#elif DV_LINUX
+        // Это имя файла
+        assert(fs::path("\\").root_name() == "");
+        assert(fs::path("\\").root_directory() == "");
+        assert(fs::path("\\").root_path() == "");
+        assert(fs::path("\\").filename() == "\\");
+#else
+    #error
+#endif
+
+#if DV_WINDOWS
         assert(fs::path("z:\\").root_name() == "z:");
         assert(fs::path("z:\\").root_directory() == "\\");
         assert(fs::path("z:\\").root_path() == "z:\\");
@@ -98,51 +128,83 @@ void test_fs_path()
         assert(fs::path("z:").root_name() == "z:");
         assert(fs::path("z:").root_directory() == "");
         assert(fs::path("z:").root_path() == "z:");
+#elif DV_LINUX
+        assert(fs::path("z:\\").root_name() == "");
+        assert(fs::path("z:\\").root_directory() == "");
+        assert(fs::path("z:\\").root_path() == "");
+
+        assert(fs::path("z:/").root_name() == "");
+        assert(fs::path("z:/").root_directory() == "");
+        assert(fs::path("z:/").root_path() == "");
+
+        assert(fs::path("z:").root_name() == "");
+        assert(fs::path("z:").root_directory() == "");
+        assert(fs::path("z:").root_path() == "");
+#else
+    #error
+#endif
 
         assert(fs::path("/").root_name() == "");
         assert(fs::path("/").root_directory() == "/");
         assert(fs::path("/").root_path() == "/");
 
+#if DV_WINDOWS
         assert(fs::path("\\").root_name() == "");
         assert(fs::path("\\").root_directory() == "\\");
         assert(fs::path("\\").root_path() == "\\");
+#elif DV_LINUX
+        assert(fs::path("\\").root_name() == "");
+        assert(fs::path("\\").root_directory() == "");
+        assert(fs::path("\\").root_path() == "");
+#else
+    #error
+#endif
 
         assert(fs::path("/привет").root_name() == "");
         assert(fs::path("/привет").root_directory() == "/");
         assert(fs::path("/привет").root_path() == "/");
 
+        // \\server\share\file
+#if DV_WINDOWS
         assert(fs::path("//сервер").root_name() == "//сервер");
         assert(fs::path("//сервер").root_directory() == "");
         assert(fs::path("//сервер").root_path() == "//сервер");
-
+#elif DV_LINUX
+        assert(fs::path("//сервер").root_name() == "");
+        assert(fs::path("//сервер").root_directory() == "/");
+        assert(fs::path("//сервер").root_path() == "/");
+#else
+    #error
+#endif
         assert(fs::path("///привет").root_name() == "");
-        assert(fs::path("///привет").root_directory() == "///");
-        assert(fs::path("///привет").root_path() == "///");
-
-        assert(fs::path("/usr///\\\\////lib") == "/usr///lib");
+        assert(fs::path("///привет").root_directory() == "/");
+        assert(fs::path("///привет").root_path() == "/");
 
 #if DV_WINDOWS
-        assert(fs::path("//\\usr\\///\\\\////lib").lexically_normal().string() == "\\usr\\lib");
+        assert(fs::path("/usr\\///\\\\////lib") == "/usr///lib");
+        assert(fs::path("//\\usr\\///\\\\////lib/\\\\").lexically_normal().string() == "\\usr\\lib\\");
 #elif DV_LINUX
-        assert(fs::path("//\\usr\\///\\\\////lib").lexically_normal().string() == "/usr/lib");
+        assert(fs::path("/usr\\///\\\\////lib") == "/usr\\/\\\\/lib");
+        assert(fs::path("//\\usr\\///\\\\////lib/\\\\").lexically_normal().string() == "/\\usr\\/\\\\/lib/\\\\");
 #else
     #error
 #endif
 
         //assert(fs::path("/привет\\hello/").root_name() == "");
         assert(fs::path("/привет\\hello/").root_directory() == "/");
+
+#if DV_WINDOWS
         assert(fs::path("\\привет\\hello/").root_directory() == "\\");
+#elif DV_LINUX
+        assert(fs::path("\\привет\\hello/").root_directory() == "");
+#else
+    #error
+#endif
+
         assert(fs::path("/привет\\hello/").root_name() == "");
 
         assert(fs::path("//привет\\hello/").root_directory() == "/");
        //assert(fs::path("//привет\\hello/").root_name() == "/");
-
-
-
-        assert(fs::path("z:/привет\\hello/").root_name() == "z:");
-        assert(fs::path("z:/привет\\hello/").root_directory() == "/");
-        assert(fs::path("z:\\привет\\hello/").root_directory() == "\\");
-        assert(fs::path("z://привет\\hello/").root_directory() == "/");
     }
 
     {
